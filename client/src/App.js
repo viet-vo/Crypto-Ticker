db1820b2bfa6c801a13fa6a6d3cd135eec3bcbe8
Viet Vo <viet.allen.qvo@gmail.com>
2021-04-28 20:09:01 -0700

    Corrected React Router package

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
+import { BrowserRouter as Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="Title">CryptoTicker</header>
-      <input type="radio" value="sa" name="gender" /> Price
-      <input type="radio" value="sa" name="gender" /> Comparison
-      <input type="radio" value="sa" name="gender" /> Graph
+      <input type="radio" value="sa" name="gender" /> 
+      <input type="radio" value="sa" name="gender" /> 
+      <input type="radio" value="sa" name="gender" /> 
+      <Link>Price</Link>
+      <Link>Comparison</Link>
+      <Link>Graph</Link>
+
+      <Switch>
+        <Route />
+        <Route />
+        <Route />
+      </Switch>
     </div>
   );
 }
